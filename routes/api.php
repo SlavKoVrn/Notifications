f5b981d0df5058448184d5d1c11c7f5fe075ffb2
SlavKoVrn <slavko.chita@gmail.com>
2026-06-16 18:49:44 +0300

    api route

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\NotificationController;
 
 // Option A: Direct definition
-Route::post('/v1/notifications/bulk', [NotificationController::class, 'bulk']);
+Route::post('/v1/notifications/bulk', [NotificationController::class, 'sendBulk']);
 
 // Option B: Using a prefix group (Recommended)
 Route::prefix('v1')->group(function () {
-    Route::post('notifications/bulk', [NotificationController::class, 'bulk']);
+    Route::post('notifications/bulk', [NotificationController::class, 'sendBulk']);
 });
